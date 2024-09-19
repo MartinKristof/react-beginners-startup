456f2a93dd7ae88096137b6a5db1e501dbc18055
Martin Krištof <martin.kristof@heureka.group>
2024-09-19 11:28:45 +0200

    feat: add routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,9 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Posts } from './posts/Posts';
 
-export const App = () => <Posts />;
+const router = createBrowserRouter([
+  { path: '/', element: <Posts /> },
+  { path: '*', element: <div>Page Not Found</div> },
+]);
+
+export const App = () => <RouterProvider router={router} />;
