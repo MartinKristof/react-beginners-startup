d1753f99105c743a744b3c074a75e915e9a05274
Martin Krištof <martin.kristof@heureka.group>
2024-09-30 14:11:34 +0200

    feat: add Spinner

--- src/app/components/PostList.tsx
+++ src/app/components/PostList.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { PostItem } from './PostItem';
 import { TPost } from '../types';
+import { Spinner } from './Spinner/Spinner';
 
 interface IPostListProps {
   posts: TPost[];
@@ -11,7 +12,7 @@ export const PostList: FC<IPostListProps> = ({ posts, isLoading = false }) => {
   let result = <div className="text-gray-500">No posts found</div>;
 
   if (isLoading) {
-    result = <div>Loading...</div>;
+    result = <Spinner />;
   } else if (posts.length > 0 && !isLoading) {
     result = (
       <ul>
